37f315b13968d95e21b5ee83a17732c2f285d511
Lindokuhle Mtengwane <lindokuhlemtengwane@gmail.com>
2024-05-09 11:19:22 +0200

    added try catch for the error messages

--- StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -1,11 +1,28 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+//const errorMessage = document.querySelector("[data-error]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   const resultValue = dividend / divider;
-  const roundedResult = Math.floor(resultValue);
-  result.innerText = roundedResult;
+
+  try {
+    if (dividend === "" || divider === "") {
+      throw new Error(
+        "Division not performed. Both values are required in inputs. Try again"
+      );
+    } else if (dividend == 20 || divider == 20) {
+      throw new Error(
+        "Division not performed. Invalid number provided. Try again"
+      );
+    } else if (dividend 
+    )
+
+    const roundedResult = Math.floor(resultValue);
+    result.innerText = roundedResult;
+  } catch (error) {
+    result.innerText = error.message;
+  }
 });
